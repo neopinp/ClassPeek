2d3703a3f99e826886a750549029384e5f8f1fbd
Wendell Mutia <89864599+Wendell-Mutia@users.noreply.github.com>
2024-10-30 20:15:59 -0400

    Added homepage

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -12,14 +12,20 @@
         <input type="text" placeholder="Search">
       </div>
       <nav>
+
+
         <section style="background-color:Orange">
           <h4>Majors</h4>
-          <ul>
-            <li>
-              <p>Example Major</p>
+          <ol>
+            <!-- Placeholder hyperlinks given a list of majors -->
+            <li id="majorli" v-for="major in maristMajors" :key="major">
+              {{ major }}
             </li>
-          </ul>
+          </ol>
         </section>
+
+
+
         <section style="background-color:Yellow">
           <h4>Professors</h4>
           <ul>
@@ -38,6 +44,55 @@ import { defineComponent } from 'vue';
 import "./styles/HomePage.css";
 export default defineComponent({
   name: 'HomePage',
+  // Placeholder data of ALL the majors in an array
+  data() {
+    return {
+      maristMajors: [
+        "Business Administration and Management",
+        "Communication",
+        "Fashion Merchandising",
+        "Psychology",
+        "Criminal Justice/Law Enforcement Administration",
+        "Computer Science",
+        "Political Science and Government",
+        "Accounting",
+        "Mass Communication/Media Studies",
+        "Biology/Biological Sciences",
+        "Education/Teaching of Individuals in Elementary Special Education Programs",
+        "Clinical Laboratory Science/Medical Technology/Technologist",
+        "Biomedical Sciences",
+        "Fashion/Apparel Design",
+        "Intermedia/Multimedia",
+        "Economics",
+        "Game and Interactive Media Design",
+        "Computer and Information Systems Security/Information Assurance",
+        "English Language and Literature",
+        "Social Work",
+        "History",
+        "Information Technology",
+        "Fine/Studio Arts",
+        "Natural Resources Management and Policy",
+        "Applied Mathematics",
+        "Mathematics Teacher Education",
+        "History Teacher Education",
+        "Multi-/Interdisciplinary Studies",
+        "Computer and Information Sciences",
+        "Philosophy",
+        "Athletic Training/Trainer",
+        "Computer Software and Media Applications",
+        "Biochemistry",
+        "Mathematics",
+        "English/Language Arts Teacher Education",
+        "Italian Language and Literature",
+        "Spanish Language and Literature",
+        "American/United States Studies/Civilization",
+        "Biology Teacher Education",
+        "Chemistry Teacher Education",
+        "Chemistry"
+      ]
+    };
+  }
 });
 </script>
   
+
