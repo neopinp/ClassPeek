c9ca75bcc4b032df71fb2780791968667cd75a9f
Luchi1324 <luchileigh8@gmail.com>
2024-11-13 12:22:27 -0500

    Changed API to work off server address

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://10.11.29.118:3000/api",
   withCredentials: true, // Automatically include cookies
   headers: {
     "Content-Type": "application/json",
