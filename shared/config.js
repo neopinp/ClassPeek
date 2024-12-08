199e8c0a8de90128cedec1b63f91da085f77a02e
Luciano Mattoli <luchileigh8@gmail.com>
2024-12-08 16:59:44 -0500

    Improved config.js comments

--- shared/config.js
+++ shared/config.js
@@ -1,13 +1,16 @@
 // Application-wide constants, lets us change the application for different servers through here
+// These specify what client (webserver) our API serves. Anything not included will be blocked by CORS in the backend.
 const CLIENT_URL = [
     'https://classpeek.ecrl.marist.edu',
     'http://localhost:8080',
     'http://localhost:3000'
 ];
 
+// Make sure the first CLIENT_URL entry is the one that hosts our Express.js server
 const API_PATH = '/api';
 const API_URL = `${CLIENT_URL[0]}${API_PATH}`;
 
+// With how our Nginx is setup, the Express.js server is hosted on the webserver and Nginx forwards requests to our API_URL to here
 const SERVER_URL = 'http://localhost:3000';
 
 // Exporting the constants for usage elsewhere in the application
